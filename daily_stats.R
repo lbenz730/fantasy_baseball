35556d12ae4c5f99613da39f0267bf7b9e71814c
lbenz730 <lukesbenz@gmail.com>
2022-04-30 23:32:29 -0400

    mv files

--- daily_stats.R
+++ daily_stats.R
@@ -11,14 +11,16 @@ source('helpers.R')
 
 df_start <- 
   tibble('matchup_id' = 1:20) %>% 
-  mutate('start_cap' = case_when(matchup_id == 1 ~ 13, 
+  mutate('start_cap' = case_when(matchup_id == 1 ~ 13,
+                                 matchup_id == 14 ~ 11,
                                  T ~ 8),
          'duration' = case_when(matchup_id == 1 ~ 11, 
+                                matchup_id == 14 ~ 14,
                                 T ~ 7),
          'end_period' = cumsum(duration),
          'start_period' = end_period - duration + 1)
 
-df_schedule <- read_csv('info/schedule.csv')
+df_schedule <- read_csv('info/schedule_2022.csv')
 
 get_daily_stats <- function(x, y, index, team) {
   if(team == 'home') {
@@ -50,12 +52,14 @@ get_daily_stats <- function(x, y, index, team) {
 
 
 get_matchup_stats <- function(week, end_early = F, season = 2022) {
+  if(season == 2021) {
+    df_schedule <- read_csv('history/schedule_2021.csv') 
+  }
   start <- df_start$start_period[week]
   end <- df_start$end_period[week]
   if(end_early) {
     end <- start + wday(Sys.Date()-2) 
   }
-  indices <- seq(1 + 6 * (week-1), 6 * week, 1)
   
   df <- 
     future_map_dfr(start:end, ~{
@@ -71,10 +75,13 @@ get_matchup_stats <- function(week, end_early = F, season = 2022) {
                  'team_id' = .y)
         }) 
       
+      indices <- which(!map_lgl(x$schedule$home$rosterForCurrentScoringPeriod$entries, is.null))
+      
       df <- 
         bind_rows(map_dfr(indices, function(i) get_daily_stats(x, y, i, 'home')),
                   map_dfr(indices, function(i) get_daily_stats(x, y, i, 'away'))) %>% 
         mutate('scoring_period_id' = .x) %>% 
+        mutate('game_id' = z$gameId) %>% 
         mutate('matchup_id' = week) %>% 
         left_join(roster_status, by = c("player", "player_id", "team_id")) %>% 
         mutate('in_lineup' = lineup_id <= 15,
@@ -89,38 +96,53 @@ get_matchup_stats <- function(week, end_early = F, season = 2022) {
   return(df)
 }
 
-df1 <- get_matchup_stats(1)
-df2 <- get_matchup_stats(2)
+df_2021 <- map_dfr(1:20, ~get_matchup_stats(.x, season = 2021))
+
+# df1 <- get_matchup_stats(1)
+# df2 <- get_matchup_stats(2)
 df3 <- get_matchup_stats(3, T)
+# 
+# 
+# bind_rows(df1, df2, df3) %>% 
+#   write_csv('history/daily_stats_2022.csv')
+# 
+# 
+# df %>% 
+#   group_by(team_id, lineup_id) %>% 
+#   summarise('ppg' = mean(points)) %>% 
+#   filter(lineup_id == 1)
+# summarise('points' = sum(points),
+#           'starts' = sum(starts))
+# 
+# 
+# 
+# df %>% 
+#   filter(in_lineup) %>% 
+#   inner_join(df_start, by = 'matchup_id') %>% 
+#   mutate('day_of_matchup' = scoring_period_id - start_period + 1) %>% 
+#   group_by(matchup_id, team_id, day_of_matchup, start_cap) %>% 
+#   summarise('day_points' = sum(points),
+#             'start_points' = sum(points[start], na.rm = T),
+#             'starts' = sum(start)) %>% 
+#   group_by(matchup_id, team_id) %>% 
+#   mutate('total_points' = cumsum(day_points),
+#          'total_starts' = cumsum(starts)) %>% 
+#   View()
 
 
-bind_rows(df1, df2, df3) %>% 
-  write_csv('history/daily_stats_2022.csv')
-
-
-df %>% 
-  group_by(team_id, lineup_id) %>% 
-  summarise('ppg' = mean(points)) %>% 
-  filter(lineup_id == 1)
-summarise('points' = sum(points),
-          'starts' = sum(starts))
+# 
 
 
 
-df %>% 
-  filter(in_lineup) %>% 
-  inner_join(df_start, by = 'matchup_id') %>% 
-  mutate('day_of_matchup' = scoring_period_id - start_period + 1) %>% 
-  group_by(matchup_id, team_id, day_of_matchup, start_cap) %>% 
-  summarise('day_points' = sum(points),
-            'start_points' = sum(points[start], na.rm = T),
-            'starts' = sum(start)) %>% 
-  group_by(matchup_id, team_id) %>% 
-  mutate('total_points' = cumsum(day_points),
-         'total_starts' = cumsum(starts)) %>% 
-  View()
-
-
-
-
+read_csv('history/daily_stats_2022.csv') %>% 
+  filter(matchup_id < 3) %>% 
+  bind_rows(df3) %>% 
+  filter(in_lineup) %>%
+  # filter(scoring_period_id == max(scoring_period_id)) %>%
+  group_by(team_id) %>% 
+  summarise('points' = sum(points)) %>% 
+  ungroup() %>% 
+  mutate('pts_back' = points - max(points)) %>% 
+  arrange(-points) %>% 
+  inner_join(teams)
 
