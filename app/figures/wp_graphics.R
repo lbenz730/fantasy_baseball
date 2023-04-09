43e3278dcae3b0dee8d723156ee65d2e15044921
lbenz730 <lukesbenz@gmail.com>
2023-04-09 16:32:34 -0400

    large files

--- app/figures/wp_graphics.R
+++ app/figures/wp_graphics.R
@@ -3,7 +3,6 @@ library(xgboost)
 library(tidymodels)
 library(lubridate)
 library(glue)
-library(ggimage)
 library(here)
 source(here('models/build_training_set.R'))
 
@@ -56,36 +55,37 @@ plot_wp <- function(season, week, plot = T, all = F) {
     df$win_prob[df$days_left == 0 & df$score_diff < 0] <- 0
   }
   
-  p <- 
-    ggplot(df, aes(x = day_of_matchup, y = win_prob)) + 
-    facet_wrap(~paste(team_home, 'vs.', team_away)) + 
-    geom_line() +
-    geom_point(aes(fill = start_factor), size = 6, color = 'black', pch = 21) +
-    geom_image(data = df_image, aes(x = 0.4, y = 0.95, image = logo_home), size = 0.125) + 
-    geom_image(data = df_image, aes(x = 0.4, y = 0.05, image = logo_away), size = 0.125) +
-    theme_bw() + 
-    theme(plot.title = element_text(size = 24, hjust = 0.5),
-          axis.title = element_text(size = 16),
-          strip.text =  element_text(size = 14),
-          plot.subtitle = element_text(size = 18, hjust = 0.5),
-          panel.grid.minor.x = element_blank(),
-          legend.position = 'bottom')  + 
-    labs(x = 'Day of Matchup',
-         y = 'Win Probability',
-         title = 'Win Probability Charts',
-         subtitle = paste('Week:', week),
-         fill = 'Start Advantage') +
-    scale_y_continuous(limits = c(0,1), labels = function(x){ paste0(100 * pmax(x, 1-x), '%') }) + 
-    scale_x_continuous(limits = c(0, max(df$days_left)), breaks = 0:max(df$days_left)) +
-    scale_fill_brewer(palette = 'RdYlGn', drop = FALSE) + 
-    guides(fill = guide_legend(nrow = 3)) 
-  
   if(plot) {
+    p <- 
+      ggplot(df, aes(x = day_of_matchup, y = win_prob)) + 
+      facet_wrap(~paste(team_home, 'vs.', team_away)) + 
+      geom_line() +
+      geom_point(aes(fill = start_factor), size = 6, color = 'black', pch = 21) +
+      ggimage::geom_image(data = df_image, aes(x = 0.4, y = 0.95, image = logo_home), size = 0.125) + 
+      ggimage::geom_image(data = df_image, aes(x = 0.4, y = 0.05, image = logo_away), size = 0.125) +
+      theme_bw() + 
+      theme(plot.title = element_text(size = 24, hjust = 0.5),
+            axis.title = element_text(size = 16),
+            strip.text =  element_text(size = 14),
+            plot.subtitle = element_text(size = 18, hjust = 0.5),
+            panel.grid.minor.x = element_blank(),
+            legend.position = 'bottom')  + 
+      labs(x = 'Day of Matchup',
+           y = 'Win Probability',
+           title = 'Win Probability Charts',
+           subtitle = paste('Week:', week),
+           fill = 'Start Advantage') +
+      scale_y_continuous(limits = c(0,1), labels = function(x){ paste0(100 * pmax(x, 1-x), '%') }) + 
+      scale_x_continuous(limits = c(0, max(df$days_left)), breaks = 0:max(df$days_left)) +
+      scale_fill_brewer(palette = 'RdYlGn', drop = FALSE) + 
+      guides(fill = guide_legend(nrow = 3)) 
+    
+    
     print(p)
+    
+    ggsave(plot = p, here(glue('figures/wp_charts/{season}/wp_chart_{season}_{week}.png')), height = 9, width = 16)
   }
   
-  
-  ggsave(plot = p, here(glue('figures/wp_charts/{season}/wp_chart_{season}_{week}.png')), height = 9, width = 16)
   
   return(select(df, 
                 team_home, team_away, day_of_matchup, 
