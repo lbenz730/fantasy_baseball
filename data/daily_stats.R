ead95f13b61b88a274c7dcff2a05788b7d567597
lbenz730 <lukesbenz@gmail.com>
2023-05-26 09:04:40 -0400

    fix rostering

--- data/daily_stats.R
+++ data/daily_stats.R
@@ -94,8 +94,8 @@ get_daily_stats <- function(x, y, index, team, df_schedule) {
   df$points[df$relief_start & df$qs] <- df$points[df$relief_start & df$qs] + 5
   
   ### Yu Darvish Rule
-  df$points[df$p_outs >= 27 & df$cg == 0] <- df$points[df$p_outs >= 27 & df$cg == 0] + 5
-  df$cg[df$p_outs >= 27 & df$cg == 0] <- 1
+  df$points[df$p_outs >= 27 & df$p_cg == 0] <- df$points[df$p_outs >= 27 & df$p_cg == 0] + 5
+  df$p_cg[df$p_outs >= 27 & df$p_cg == 0] <- 1
   
   return(df)
 
