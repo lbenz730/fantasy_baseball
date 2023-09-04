f40246e9a66ab9705c45f9b0ec9c3aeb529daca5
lbenz730 <lukesbenz@gmail.com>
2023-09-04 09:29:20 -0400

    playoff fix

--- update_data.R
+++ update_data.R
@@ -706,7 +706,7 @@ champions <-
   dplyr::slice(1:4) %>% 
   group_split() %>% 
   future_map_chr(~{
-    if(params$matchup_id < 22) {
+    if(params$matchup_id < max(df_start$matchup_id)) {
       championship_sim(.x$team, team_mus, team_sigmas, matchup_id = params$matchup_id, wp = df_wp$win_prob[1:2])
     } else {
       championship_sim(c(df_wp$team_home[1], df_wp$team_away[1]), team_mus, team_sigmas, matchup_id = params$matchup_id, wp = df_wp$win_prob[1])
